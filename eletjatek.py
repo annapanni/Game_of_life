f1cb5b588ed801f6fbaa817313eda135550328f4
Mészáros Anna Veronika <meszaros.annapanni@gmail.com>
2018-05-07 19:44:38 +0200

    Képek hozzáadása

--- eletjatek.py
+++ eletjatek.py
@@ -1,57 +1,69 @@
-
 import sys, pygame, random
 
 pygame.init()
 
-size = width, height = 100, 100
-speed = [0, 0]
+cell_width=10
+cell_height=10
+grid=grid_x, grid_y=100,100
+size = width, height = cell_width*grid_x, cell_height*grid_y
 black = 0, 0, 0
 white = 255, 255, 255
+##üres grid
+grid=[]
+for i in range(grid_y):
+    grid.append([0]*grid_x)
 
 screen = pygame.display.set_mode(size)
+sejt=pygame.image.load("sejt.png")
+sejt2=pygame.image.load("sejt2.png")
+sejt3=pygame.image.load("sejt3.png")
+sejt4=pygame.image.load("sejt4.png")
 
-def foglalt (screen, poz):
-    if poz[0] in range(width) and poz[1] in range(height):
-        if screen.get_at(poz)==white:
+def foglalt (grid, poz):
+    x=poz[0]
+    y=poz[1]
+    if x in range(grid_x) and y in range(grid_y):
+        if grid[x][y]!=0:
             return True
     return False
 
-def szomszéd (screen, poz):
+def szomszéd (grid, poz):
     sz=0
     x=poz[0]
     y=poz[1]
 
-    if foglalt(screen,(x+1,y)):
+    if foglalt(grid,(x+1,y)):
         sz=sz+1
 
-    if foglalt(screen,(x-1,y)):
+    if foglalt(grid,(x-1,y)):
         sz=sz+1
 
-    if foglalt(screen,(x+1,y+1)):
+    if foglalt(grid,(x+1,y+1)):
         sz=sz+1
 
-    if foglalt(screen,(x+1,y-1)):
+    if foglalt(grid,(x+1,y-1)):
         sz=sz+1
 
-    if foglalt(screen,(x-1,y+1)):
+    if foglalt(grid,(x-1,y+1)):
         sz=sz+1
 
-    if foglalt(screen,(x-1,y-1)):
+    if foglalt(grid,(x-1,y-1)):
         sz=sz+1
 
-    if foglalt(screen,(x,y+1)):
+    if foglalt(grid,(x,y+1)):
         sz=sz+1
 
-    if foglalt(screen,(x,y-1)):
+    if foglalt(grid,(x,y-1)):
         sz=sz+1
     return sz
 ##inicializálás (ősleves)
-for x in range(width):
-        for y in range(height):
+for x in range(grid_x):
+        for y in range(grid_y):
             if random.random()<0.7:
-                screen.set_at((x,y),black)
+                grid[x][y]=0
             else:
-                screen.set_at((x,y),white)
+                grid[x][y]=1
+          
 
 while 1:
     halottak=[]
@@ -59,19 +71,35 @@ while 1:
         if event.type == pygame.QUIT: sys.exit()
     szület=[]
     halott=[]
-    for x in range(width):
-        for y in range(height):
-            szdb=szomszéd(screen,(x,y))
-            if foglalt(screen,(x,y)):
+    for x in range(grid_x):
+        for y in range(grid_y):
+            szdb=szomszéd(grid,(x,y))
+            if foglalt(grid,(x,y)):
                 if szdb>3 or szdb<2:
                     halott.append((x,y))
+                else:
+                    if grid[x][y]<13:
+                        grid[x][y]+=1
             else:
-                if szomszéd(screen,(x,y))==3:
+                if szomszéd(grid,(x,y))==3:
                     szület.append((x,y))
 
-    for a in halott:
-        screen.set_at(a,black)
-    for a in szület:
-        screen.set_at(a,white) 
+    for x,y in halott:
+        grid[x][y]=0
+    for x,y in szület:
+        grid[x][y]=1
     
+    screen.fill(black)
+    for x,oszlop in enumerate(grid):
+        for y,s in enumerate(oszlop):
+            if s in [1,2,3]:
+                screen.blit(sejt, [x*cell_width,y*cell_height])
+            elif s in [4,5,6]:
+                screen.blit(sejt2, [x*cell_width,y*cell_height])
+            elif s in [7,8,9]:
+                screen.blit(sejt3, [x*cell_width,y*cell_height])
+            elif s>10:
+                screen.blit(sejt4, [x*cell_width,y*cell_height])
+                
+    print(grid)
     pygame.display.flip()
